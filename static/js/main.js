f2d8a2e880a43119e5535ee4565a08b3637d9fe0
Jamie <fractalwrench@gmail.com>
2017-12-28 14:16:53 +0000

    remove jquery dep

--- static/js/main.js
+++ static/js/main.js
@@ -2,23 +2,22 @@
 
 var main = {
   init : function() {
-    // Shorten the navbar after scrolling a little bit down
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar").addClass("top-nav-short");
-        } else {
-            $(".navbar").removeClass("top-nav-short");
-        }
-    });
+      window.addEventListener('scroll', function(e) {
+          var navbar = document.getElementById("navbar-container");
 
-    // On mobile, hide the avatar when expanding the navbar menu
-    $('#main-navbar').on('show.bs.collapse', function () {
-      $(".navbar").addClass("top-nav-expanded");
-    });
-    $('#main-navbar').on('hidden.bs.collapse', function () {
-      $(".navbar").removeClass("top-nav-expanded");
-    });
+          if (calculateHeightOffset(navbar) > 50) {
+              navbar.classList.add("top-nav-short");
+          } else {
+              navbar.classList.remove("top-nav-short");
+          }
+      });
   }
 };
+
+// Native
+function calculateHeightOffset (element) {
+  return element.getBoundingClientRect().top
+  + window.pageYOffset - document.documentElement.clientTop;
+}
 
 document.addEventListener('DOMContentLoaded', main.init);
